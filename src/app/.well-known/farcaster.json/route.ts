790f6e11a0673614d471196f35bf55a84534936f
KingAceBlack <119745116+KingAceBlack@users.noreply.github.com>
2025-07-31 16:21:01 -0700

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,11 +3,7 @@ export async function GET() {
   // const appUrl = 'https://9cf4c62f-7bc5-42d6-a3d0-01aeaea50754-00-2obuwftnnc76u.kirk.replit.dev';
   const appUrl = 'https://based-dungeons.vercel.app';
 
-  // "accountAssociation": {
-    //   "header": "eyJmaWQiOjIxMzEwMywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEVhQTJGOGIwOTEzNTI3ZDYyNzE5MTVkNzQ1MDdlMDdFNmUzMjRmNDAifQ",
-    //   "payload": "eyJkb21haW4iOiI5Y2Y0YzYyZi03YmM1LTQyZDYtYTNkMC0wMWFlYWVhNTA3NTQtMDAtMm9idXdmdG5uYzc2dS5raXJrLnJlcGxpdC5kZXYifQ",
-    //   "signature": "MHhhY2JjZjFlOWVhM2E0NDM2Nzc1M2Y3YTM5NDRkYzQ4ZTM2MDIyODdiMTg5N2VlOGQ2NjAwNWZhZjViYjMwNzBlNmVhMDVhNjljNzBlNDQ2ODFkNzg1YzdiMmE1MmQ3MjZjOTA1YjliMDU0MTRiNGM5OTE2NjAyNDhlZDk4YzJiNzFj"
-  // },
+
 
   const config = {
     "accountAssociation": {
@@ -16,17 +12,7 @@ export async function GET() {
     "signature": "S6m54usU3ihFLopJVHLEGKOslUn+851JvpobD7x7bVlPNhIO17iSfOeActc9h7ZFgJ3NoP3ZCAiQOsdcUhyrVRs="
       
   },
-    "frame": {
-      "version": "1",
-      "name": "Based Dungeon",
-      "iconUrl": `${appUrl}/crossedSwordsLogo.png`,
-      "homeUrl": appUrl,
-      "imageUrl": `${appUrl}/crossedSwords.png`,
-      "buttonTitle": "Play",
-      "splashImageUrl": `${appUrl}/crossedSwordsLogo.png`,
-      "splashBackgroundColor": "#EAE3C8",
-      "webhookUrl": `${appUrl}/api/webhook`
-    }
+   
   };
 
   return Response.json(config);
