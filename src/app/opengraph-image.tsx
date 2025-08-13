61012d4d5dde7af589be228f128555323ce8ae95
KingAceBlack <119745116+KingAceBlack@users.noreply.github.com>
2025-08-13 15:52:04 -0700

    Update opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,7 @@
 
 import { ImageResponse } from "next/og";
 
-export const alt = "Fell The Dragon";
+export const alt = "Movie Quest";
 export const size = {
   width: 600,
   height: 400,
@@ -13,7 +13,7 @@ export default async function Image() {
   return new ImageResponse(
     (
       <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-6xl">Fell The Dragon</h1>
+        <h1 tw="text-6xl">Movie Quest</h1>
       </div>
     ),
     {
